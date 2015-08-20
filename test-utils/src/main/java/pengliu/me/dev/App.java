1ad43bed1602af317e6f07ec0e8e032f28347760
liupeng330 <330liupeng@gmail.com>
2015-08-20 22:45:53 +0800

    init

---
+++ test-utils/src/main/java/pengliu/me/dev/App.java
@@ -0,0 +1,18 @@
+package pengliu.me.dev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static void output_dev()
+    {
+        System.out.println("hello dev");
+    }
+}
---
+++ untitled/src/test/java/pengliu/me/mytestjar/test_utils.java
@@ -0,0 +1,12 @@
+package pengliu.me.mytestjar;
+
+/**
+ * Created by peng on 8/20/15.
+ */
+public class test_utils
+{
+    public static void output()
+    {
+        System.out.println("testing for utils");
+    }
+}
